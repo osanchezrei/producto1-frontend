ae108f19573a78b9b8832167367cddb2be22e84e
osanchezrei <osanchezrei@uoc.edu>
2022-10-19 20:45:44 +0200

    Añadidos componentes y routing, se muestra el componente detalle correctamente

--- producto1/src/app/app-routing.module.ts
+++ producto1/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetalleComponent } from './detalle/detalle.component';
+import { ListadoComponent } from './listado/listado.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ReproductorComponent } from './reproductor/reproductor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'listado', 
+    component: ListadoComponent,
+    children: [
+      {
+        path: 'detalle', 
+        component: DetalleComponent,
+        children: [
+          {path: 'reproductor', component: ReproductorComponent}
+      ]}      
+    ]},
+  {path: '', redirectTo: '/listado', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- producto1/src/app/app.module.ts
+++ producto1/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListadoComponent } from './listado/listado.component';
+import { DetalleComponent } from './detalle/detalle.component';
+import { ReproductorComponent } from './reproductor/reproductor.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoComponent,
+    DetalleComponent,
+    ReproductorComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
