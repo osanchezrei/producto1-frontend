002b918dcb6b50189ce10fe46456a295d963e313
Jaume Tarradas Llort <jaume@bic.cat>
2022-10-23 16:13:16 +0200

    component detalles

--- producto1/src/app/app-routing.module.ts
+++ producto1/src/app/app-routing.module.ts
@@ -1,18 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetalleComponent } from './detalle/detalle.component';
-import { JugadoresComponent } from './jugadores/jugadores.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ReproductorComponent } from './reproductor/reproductor.component';
 
 const routes: Routes = [
-  {
-    path: 'listado',
-    component: JugadoresComponent,
-    children: [
-      {path: 'detalle', component: DetalleComponent},
-    ]},
-  {path: '', redirectTo: '/listado', pathMatch: 'full'},
+  {path: '', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent}
 ];
 
