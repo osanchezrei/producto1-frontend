bcded651ad1b625d11932878b5b5c0101656c8a9
Jaume Tarradas Llort <jaume@bic.cat>
2022-10-27 08:14:59 +0200

    set up audio n video playback

--- producto1/src/app/components/reproductor/reproductor.component.ts
+++ producto1/src/app/components/reproductor/reproductor.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-reproductor',
   templateUrl: './reproductor.component.html',
   styleUrls: ['./reproductor.component.css']
 })
-export class ReproductorComponent implements OnInit {
+export class ReproductorComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  @Input() audio?: string;
+  @Input() video?: string;
 
 }
